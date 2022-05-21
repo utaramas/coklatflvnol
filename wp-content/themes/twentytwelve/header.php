310ef36bec7ca32776f8798f1278ebd6cf6d14a7
onlineservicemarketing <93332126+utaramas@users.noreply.github.com>
2022-05-21 21:05:10 +0700

    Update header.php

--- wp-content/themes/twentytwelve/header.php
+++ wp-content/themes/twentytwelve/header.php
@@ -21,6 +21,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
+	<meta name="google-site-verification" content="ockqUscGpnWv0gEPtVpqPuX4jLTKcNivLeG_75wRUd4" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="https://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
